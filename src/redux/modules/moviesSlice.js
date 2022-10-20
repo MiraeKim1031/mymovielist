b4a520343bf95503e558d8623066759b4e2280d0
JeongWonJi <jungwonji1960@gmail.com>
2022-10-20 18:24:12 +0900

    env수정

--- src/redux/modules/moviesSlice.js
+++ src/redux/modules/moviesSlice.js
@@ -13,7 +13,7 @@ export const __getMovies = createAsyncThunk(
   async (payload, thunkAPI) => {
     try {
       console.log("*********여기는겟무비");
-      const data = await axios.get(`${process.env.REACT_APP_MOVIES}`);
+      const data = await axios.get(process.env.REACT_APP_MOVIES);
       // const data = await axios.get(
       //   "https://agile-bastion-29157.herokuapp.com/movies"
       // );
