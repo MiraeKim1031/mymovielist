fb81e6a12cbac993c66d312824b7e43294675131
JeongWonJi <jungwonji1960@gmail.com>
2022-10-20 18:45:16 +0900

    env수정

--- src/redux/modules/moviesSlice.js
+++ src/redux/modules/moviesSlice.js
@@ -1,7 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
 const initialState = {
   movies: [],
