30f4b74e516de94c3289db4573d1198a36d3c6f6
JeongWonJi <jungwonji1960@gmail.com>
2022-10-20 14:33:59 +0900

    initial commit

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import movies from '../modules/moviesSlice';
-import movie from '../modules/movieSlice';
-import comments from '../modules/commentsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import movies from "../modules/moviesSlice";
+import movie from "../modules/movieSlice";
+import comments from "../modules/commentsSlice";
 
 const store = configureStore({
-   reducer: { movies: movies, movie: movie, comments: comments },
+  reducer: { movies: movies, movie: movie, comments: comments },
 });
 
 export default store;
