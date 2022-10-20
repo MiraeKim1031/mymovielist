25ae34dd53d49fded46dad71f3e8c01783d02ba2
JeongWonJi <jungwonji1960@gmail.com>
2022-10-20 16:50:29 +0900

    env 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,72 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import Layout from '../components/Layout';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Layout from "../components/Layout";
 
 const Home = () => {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   return (
-      <Layout>
-         <FirstWords><h2>영화를 기록해볼까요?</h2></FirstWords>
-         <StContainer>
-            <MainBox
-               onClick={() => {
-                  navigate('/write');
-               }}>
-               <p>
-                  📝
-                  <br />
-                  작성할 결심
-               </p>
-            </MainBox>
-            <MainBox
-               onClick={() => {
-                  navigate('/movies');
-               }}>
-               <p>
-                  🎥
-                  <br />
-                  영화 리스트
-               </p>
-            </MainBox>
-         </StContainer>
-      </Layout>
-   );
+  return (
+    <Layout>
+      <FirstWords>
+        <h2>영화를 기록해볼까요?</h2>
+      </FirstWords>
+      <StContainer>
+        <MainBox
+          onClick={() => {
+            navigate("/write");
+          }}
+        >
+          <p>
+            📝
+            <br />
+            작성할 결심
+          </p>
+        </MainBox>
+        <MainBox
+          onClick={() => {
+            navigate("/moviepage");
+          }}
+        >
+          <p>
+            🎥
+            <br />
+            영화 리스트
+          </p>
+        </MainBox>
+      </StContainer>
+    </Layout>
+  );
 };
 
 export default Home;
 
 const StContainer = styled.div`
-   height: 100%;
-   display: flex;
-   padding: 30px;
-   align-items: center;
-   justify-content: center;
-   gap: 40px;
-   font-size: 30px;
-   display: flex;
+  height: 100%;
+  display: flex;
+  padding: 30px;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+  font-size: 30px;
+  display: flex;
 `;
 
 const MainBox = styled.div`
-   height: 300px;
-   width: 400px;
-   border-radius: 20px;
-   border: 0.5px solid #97a8a3;
-   text-align: center;
-   cursor: pointer;
-   background-color: #f8efe4;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   color: #203148;
+  height: 300px;
+  width: 400px;
+  border-radius: 20px;
+  border: 0.5px solid #97a8a3;
+  text-align: center;
+  cursor: pointer;
+  background-color: #f8efe4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #203148;
 `;
 
 const FirstWords = styled.div`
-   margin-top: 60px;
-   text-align: center;
+  margin-top: 60px;
+  text-align: center;
 `;
